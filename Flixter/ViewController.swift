9f9163e7c44d59536052f378637c86ab27de554c
Tony <tony.fu@stonybrook.edu>
2021-02-19 16:53:29 -0500

    Initial Commit

---
+++ Flixter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flixter
+//
+//  Created by Tony on 2/19/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
